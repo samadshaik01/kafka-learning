477cf3b0ebc810bf06b6f0226f43b7d742f26684
samad shaik <samad.shaik@hotmail.com>
2025-08-03 20:17:01 +0530

    manual ConsumerFactory configuration

---
+++ consumer-service/src/main/java/org/example/consumerservice/config/ConsumerProperties.java
@@ -0,0 +1,42 @@
+package org.example.consumerservice.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.example.commonlibrary.entity.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ConsumerProperties {
+
+
+    @Bean
+    public ConsumerFactory<String, User> consumerFactory() {
+        JsonDeserializer<User> deserializer = new JsonDeserializer<>(User.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("org.example.commonlibrary"); // no dash!
+        deserializer.setUseTypeMapperForKey(false);
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "user_group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, User> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, User> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
